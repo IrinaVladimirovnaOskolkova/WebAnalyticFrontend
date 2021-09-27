695157d05c84addd6ced46185ef5f9eafff10dea
IrinaVladimirovnaOskolkova <oskolkova.irina.v@gmail.com>
2021-09-27 22:19:39 +0300

    ready-made version of the registration page

--- app/script.js
+++ app/script.js
@@ -16,9 +16,20 @@ let inputPassword = document.getElementById("InputPassword");
 let inputPassConfirm = document.getElementById("InputPassConfirm");
 let message = document.getElementById("message");
 let btnReg = document.getElementById("btnReg");
+let modal = document.getElementById("Modal");
+let btnModalClose = document.getElementById("btnModalClose");
+let serverResponse =document.getElementById("serverResponse");
 let nameCorrect = false;
 let loginCorrect = false;
 let passwordCorrect = false;
+
+
+let temp = `{
+  "actionStatus":{
+  "message":"Успешная регистрация",
+  "status":true
+  }
+}`;
 
 window.onload = function () {
   validationName();
@@ -62,3 +73,40 @@ function fullFieldValidation() {
   else tunrOffBtnReg();
 }
 // Нажатие на кнопку "зарегистрироваться"
+
+btnReg.addEventListener("click", function(){
+  let name = 'userName=' + encodeURIComponent(inputName.value);
+  let login = 'userLogin=' + encodeURIComponent(inputLogin.value);
+  let password = 'userPassword=' + encodeURIComponent(inputPassword.value);
+
+  let request = new XMLHttpRequest();
+  request.open('POST', 'https://jsonplaceholder.typicode.com/users', true);
+  request.addEventListener('readystatechange', function(){
+    if(request.readyState == 4) {
+      if(request.status == 201){
+        let obj = JSON.parse(temp);
+        serverResponse.innerHTML = obj.actionStatus.message;
+
+        if(obj.actionStatus.status == true) {
+          modal.style.backgroundColor = "#939699";
+          btnModalClose.innerHTML = "Авторизоваться";
+          btnModalClose.onclick = () => {
+            document.location.href = "authorization.html"
+          };
+          window.onclick = () => {
+            btnModalClose.onclick();
+            window.onclick = null;
+            message.innerHTML = "";
+          };
+
+        }
+      }
+      else {
+        serverResponse.innerHTML = 'Error: ' + request.status;
+      }
+    }
+  });
+
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  request.send(name, login, password);
+});
