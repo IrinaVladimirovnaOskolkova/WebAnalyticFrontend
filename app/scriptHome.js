162ad7cbacf5671e13ac9e32a19f070f3916d1dd
IrinaVladimirovnaOskolkova <oskolkova.irina.v@gmail.com>
2021-09-30 21:35:37 +0300

    implementation of filling the modal window with users

--- app/scriptHome.js
+++ app/scriptHome.js
@@ -1,8 +1,8 @@
 // *********************** ГЛАВНАЯ СТРАНИЦА *************************
-// Заполнение таблицы
 window.onload = () => {
-    loadWebSiteList(); 
+    loadWebSiteList();
 }
+// Заполнение таблицы
 let siteJson = `{
     "webSiteList":
     [{"site":96},
@@ -15,11 +15,66 @@ function loadWebSiteList(){
         
   for(let item of obj.webSiteList){
     for (i in item){
-      tableBody.innerHTML += '<tr><td width="90%"><a href="/" id="' + item[i] + '">' 
+      tableBody.innerHTML += '<tr><td width="90%"><a href="/?id=' + item[i] + '">' 
       + i + '</td><td width="10%" class="text-center admin">\
-      <a data-bs-toggle="modal" data-bs-target="#ModalHome"><i class="fa fa-user-o" aria-hidden="true">\
+      <a data-bs-toggle="modal" id="btnUser" data-bs-target="#ModalHome"><i class="fa fa-user-o" aria-hidden="true">\
       </i></a></td>'
     }
   }
+
+  loadUserList();
 }
 // Заполнение и редактировани модального окна
+let userJson = `{
+  "userListPermission":
+  [{"vasya":55,"access":true},{"petya":75,"access":true},{"nikolas":117,"access":false}]
+}`;
+
+function loadUserList(){
+  let btnUser = document.getElementById("btnUser");
+
+  btnUser.onclick = () => {
+  let modalBody = document.getElementById("modalBody");
+  modalBody.innerHTML = "";
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4){
+    if(xhr.status === 200){
+      let obj = JSON.parse(userJson);
+      for(let item of obj.userListPermission){
+
+        let name;
+        let id;
+        let state = item["access"];
+        for (let key in item){
+          name = key;
+          id = item[name];
+          break;
+        }
+
+        if(state == true) {
+          modalBody.innerHTML += `<div class="form-check form-switch">
+          <label class="form-check-label"> ${name}  </label>
+          <input class="form-check-input" type="checkbox" checked/>
+          <input type="hidden" name="id" value="${id}"></div>`;
+        }
+        else {
+          modalBody.innerHTML += `<div class="form-check form-switch">
+          <label class="form-check-label"> ${name}  </label>
+          <input class="form-check-input" type="checkbox"/>
+          <input type="hidden" name="id" value="${id}"></div>`;
+        }
+
+
+      }
+    }
+    else{
+      modalBody.innerHtml = "Error:" + xhr.status + "!"; 
+    }
+  }  
+}
+xhr.send();
+}
+}
